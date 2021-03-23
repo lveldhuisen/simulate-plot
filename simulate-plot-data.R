fa13711635c9cbdd60e077f0e1dc23320c8d10b1
Leah Veldhuisen <lveldhuisen@email.arizona.edu>
2021-03-23 11:29:03 -0600

    increase noise in Y value

---
+++ simulate-plot-data.R
@@ -0,0 +1,11 @@
+#simulate and plot data 
+#Leah Veldhuisen 
+#lveldhuisen@email.arizona.edu
+#2021-03-23
+
+#simulate data from normal distribution 
+x <- rnorm(n = 100)
+y <- 2 * x + rnorm(n = 100, sd = 0.1)
+
+#plot simulated data
+plot(x = x, y = y)
